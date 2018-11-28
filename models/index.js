2bf0d86e3feca53428441d5363397a267e483366
BrokenFingers <rabtsevichandrey11@gmail.com>
2018-11-28 23:26:15 +0300

    done

---
+++ models/index.js
@@ -0,0 +1,24 @@
+module.exports = (Sequelize, config) => {
+    const sequelize = new Sequelize(config.db, config.login, config.password, {
+        host: config.host,
+        dialect: config.dialect,
+        define : {
+            timestamps : true,
+            paranoid : true
+        }
+    });
+
+    const Fleet = require('../models/fleets')(Sequelize, sequelize);
+    const Motion = require('../models/motions')(Sequelize, sequelize);
+    const Vehicle = require('../models/vehicles')(Sequelize, sequelize);
+    const Auth = require('../models/managers')(Sequelize, sequelize);
+
+    Vehicle.hasMany(Fleet, {foreignKey: 'fleetId', sourceKey: 'id'});
+    Motion.hasMany(Vehicle, {foreignKey: 'vehicleId', sourceKey: 'id'});
+
+    return {
+        Fleet, Motion, Vehicle, Auth,
+        sequelize: sequelize,
+        Sequelize: Sequelize,
+    };
+};
